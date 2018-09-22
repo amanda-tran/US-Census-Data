5888e6ddd25cd936a71745f703f819c88f32eff7
Amanda <tranamanda14@gmail.com>
2018-09-22 05:39:56 -0500

    t

--- app/public/assets/js/API-event-handling.js
+++ app/public/assets/js/API-event-handling.js
@@ -1,37 +1,44 @@
-// Code here handles queries for specific city census data in the database
-// In this case, the user submits a city name. That city name is passed as a
-// URL parameter. Our server then performs the search to grab that character from the Database.
+// testNumber is to help us test the graph to make sure the bars are changing height when search is pressed
+var testNumber = 0;
 
 // when user hits the city-search-btn
 $("#search-btn").on("click", function () {
-  event.preventDefault();
-  // save the character they typed into the character-search input
-  let searchedCity = $("#city-search")
-    .val()
-    .trim();
 
-  // Using a RegEx Pattern to remove spaces from searchedCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  // increase testNumber by 5 whenever seasrch button is pressed
+  testNumber += 5;
+
+  event.preventDefault();
+
+  // Saves the search value from the city-search bar into a variable.
+  let searchedCity = $("#city-search").val().trim();
   searchedCity = searchedCity.replace(/\s+/g, "").toLowerCase();
 
-  // run an AJAX GET-request for our servers api,
-  // including the user's character in the url
   $.get("/api/" + searchedCity, function (data) {
-    // log the data to our console
-    console.log(data);
-    // empty to well-section before adding new content
+    // For now let's just work with 1 bar in our graph.
+    var exampleData = testNumber // since our api doesn't work, we'll use some example data - this example data will just be our testNumber
+    console.log(exampleData);
+
+    // ########### code already in place before we started changing it ##############################################
     $("#test-data-dump").empty();
-    // if the data is not there, then return an error message
     if (!data) {
-      $("#test-data-dump").append("<h2> No data found. </h2>");
+      $("#test-data-dump").append("<h2> Hmmm... No data was returned from database. Try another city. </h2>");
     } else {
-      // otherwise
-      // append the character name
-      $("#test-data-dump").append("<h2>" + data.Areaname + "</h2>");
-      // the role
-      $("#test-data-dump").append("<h3>Data Set #1: " + data.STCOU + "</h3>");
-      // the age
-      $("#test-data-dump").append("<h3>Data Set #2: " + data.PST100209D + "</h3>");
+      $("#test-data-dump").append("<h2> Areanam:" + data.areaname + "</h2>");
+      $("#test-data-dump").append("<h3>Data Set #1: " + data.stateCounty + "</h3>");
+      $("#test-data-dump").append("<h3>Data Set #2: " + data.totalPopulation + "</h3>");
+      $("#percentPopulationOver65").append(`<span class="bar-fill" style="width:${data.populationOver65}%;"></span>`);
+      $("#unemploymentRate2010").append(`<span class="bar-fill" style="width:${data.unemploymentRate2010}%;"></span>`);
+      // ##############################################################################################################
+
+      // Appending a place-holder bar in our graph into the repVote1992 element
+      // this will act as the 1 bar in our graph we'll do for now.
+      $("#repVote1992").append(`<div class="bar bar-1 stat-1" style="height:0%;"></div>`);
+
+      // This changes the CSS styling of the bar so that the bar's height is X% of the height of the parent element
+      $("#repVote1992").css("height", exampleData + "%");
+
+      // Done - run this and check that everytime you press SEARCH it'll make the graph taller.
+      //okay going back to line 67 ^ 8===================D     ~~~################################ ~ ----------------------------------------------------------> ~~~~~~~~~~~~~~~~~~~~
     }
   });
 });
